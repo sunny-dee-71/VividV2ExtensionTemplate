a945395661f1adc989ea12ae5c3ca9f5437ff33a
Sunny_dee <sunnydee148@gmail.com>
2026-02-20 22:09:13 +0000

    Add project files.

---
+++ Mods/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VividV2.Classes.Buttons;
+
+namespace VividV2ExtensionTemplate.Mods
+{
+    internal class Categories
+    {
+        public static readonly Category Movement = Category.Register("Movement");
+        public static readonly Category Visual = Category.Register("Visual");
+    }
+}
---
+++ Mods/Modules/Speedboost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VividV2.Classes.Buttons;
+using VividV2.Mods;
+
+namespace VividV2ExtensionTemplate.Mods.Modules
+{
+    internal class Speedboost : Module
+    {
+        private Variable SpeedBoostAmount = new Variable("Speedboost Amount", 1.0f, 0.0f, 10.0f);
+        public Speedboost() : base("Speedboost", Categories.Movement)
+        {
+            AddVariable(SpeedBoostAmount);
+        }
+    }
+}
