c54b40e09dd0912972b3f7d470863d6b52f80a53
Sunny_dee <sunnydee148@gmail.com>
2026-02-21 12:45:54 +0000

    add comments to help and aslo changed file extension to ".vivid".

--- Mods/Categories.cs
+++ Mods/Categories.cs
@@ -7,6 +7,7 @@ namespace VividV2ExtensionTemplate.Mods
 {
     internal class Categories
     {
+        // to create categories just copy these and change the variable name and the string passed.
         public static readonly Category Movement = Category.Register("Movement");
         public static readonly Category Visual = Category.Register("Visual");
     }
--- Mods/Modules/Speedboost.cs
+++ Mods/Modules/Speedboost.cs
@@ -8,10 +8,48 @@ namespace VividV2ExtensionTemplate.Mods.Modules
 {
     internal class Speedboost : Module
     {
-        private Variable SpeedBoostAmount = new Variable("Speedboost Amount", 1.0f, 0.0f, 10.0f);
-        public Speedboost() : base("Speedboost", Categories.Movement)
+
+        // to initialize the module do this
+        // the first string will be the name shown in the menu
+        // the second is the category, that is where the mod will be located
+        // the third bool is if it is toggleable, set to true to be toggleable or false to not be toggleable
+        // if it is toggleable OnEnable will be called each time it is pressed
+        // if it is toggleable OnDisable will never be called
+        // Update will will always be called no matter if it is or isnt toggleable
+        public Speedboost() : base("Speedboost", Categories.Movement, true)
         {
+            // to add variables first initialize the variable
+            Variable SpeedBoostAmount = new Variable("Speedboost Amount", 1.0f, 0.0f, 10.0f);
+            // and the add it to the module by using AddVariable
             AddVariable(SpeedBoostAmount);
+
+            // you can get variables by using GetVariable with the name (WARNING: the name is case sensitive)
+            GetVariable("Speedboost Amount");
         }
+
+
+        public override void Update()
+        {
+            // this will run every frame
+
+            if (Enabled)
+            {
+                // use the Enabled boolean to check weather the mod is enabled in update
+            }
+        }
+
+        public override void OnDisable()
+        {
+            // this will be called every time the module is disabled
+        }
+
+        public override void OnEnable()
+        {
+            // this will be called every time the module is enabled
+        }
+
+        // other useful methods are SetEnabled which will set the mod the be enabled or disabled
+
+
     }
 }
