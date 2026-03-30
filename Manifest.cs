397e09cbfa5a4afdbb9a18021a096a3f75184615
Sunny_dee <sunnydee148@gmail.com>
2026-03-30 22:17:27 +0100

    Added Manifest.cs

---
+++ Manifest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VividV2.Classes;
+
+namespace VividV2.Mods
+{
+    internal class Manifest : ExtensionManifest
+    {
+        public override string Name { get; set; } = "VividExtension";
+        public override string Version { get; set; } = "1.0.0";
+
+    }
+}
